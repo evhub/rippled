8b4da6476430f9c4f6fc8c3cc48717ce304adad9
Tom Ritchford <tom@swirly.com>
2014-04-01 20:29:57 -0400

    Extract off more general-purpose code.

--- src/beast/python/beast/util/File.py
+++ src/beast/python/beast/util/File.py
@@ -2,11 +2,26 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os
 
+def first_line_starting_with(filename, prefix):
+  with open(filename, 'r') as contents:
+    for line in contents:
+      line = line.strip()
+      if line.startswith(prefix):
+        return line[len(prefix):].split()
+  return []
+
 def find_files_with_suffix(base, suffix):
     for parent, _, files in os.walk(base):
         for path in files:
             path = os.path.join(parent, path)
             if path.endswith(suffix):
-                yield path
+                yield os.path.normpath(path)
 
+def child_files(parent, files):
+  return [os.path.normpath(os.path.join(parent, f)) for f in files]
 
+def sibling_files(path, files):
+  return child_files(os.path.dirname(path), files)
+
+def replace_extension(file, ext):
+  return os.path.splitext(file)[0] + ext
--- src/beast/python/beast/util/Tests.py
+++ src/beast/python/beast/util/Tests.py
@@ -7,55 +7,26 @@ from beast.util import File
 LIBS_PREFIX = '// LIBS:'
 MODS_PREFIX = '// MODULES:'
 
-def first_line_starting_with(filename, prefix):
-  with open(filename, 'r') as contents:
-    for line in contents:
-      line = line.strip()
-      if line.startswith(prefix):
-        return line[len(prefix):].split()
-  return []
-
-def get_libs(path):
-    """Returns the list of libraries needed by the test source file. This is
-       accomplished by scanning the source file for a special comment line
-       with this format, which must match exactly:
-
-       // LIBS: <name>...
-
-       path = path to source file"""
-
-    return first_line_starting_with(path, LIBS_PREFIX)
-
-def get_source_modules(path):
-    """Returns the list of source modules needed by the test source file.
-
-     // MODULES: <module>...
-
-     path = path to source file."""
-
-    items = first_line_starting_with(path, MODS_PREFIX)
-    parent = os.path.dirname(os.path.normpath(path))
-    return [os.path.join(parent, i) for i in items]
-
 def build_executable(env, path, main_program_file):
     """Build a stand alone executable that runs
        all the test suites in one source file."""
-    libs = get_libs(path)
-    source_modules = get_source_modules(path)
+    libs = File.first_line_starting_with(path, LIBS_PREFIX)
+    source_modules = File.first_line_starting_with(path, MODS_PREFIX)
+    source_modules = File.sibling_files(path, source_modules)
+
     bin = os.path.basename(os.path.splitext(path)[0])
     bin = os.path.join('bin', bin)
-    srcs = [main_program_file, path] + (source_modules or [])
 
     # All paths get normalized here, so we can use posix
     # forward slashes for everything including on Windows
-    srcs = [os.path.normpath(os.path.join('bin', x)) for x in srcs]
-    objs = [os.path.splitext(x)[0] + '.o' for x in srcs]
-    env_ = env
+    srcs = File.child_files('bin', [main_program_file, path] + source_modules)
+    objs = [File.replace_extension(f, '.o') for f in srcs]
     if libs:
-        env_.Append(LIBS=libs)
-    env_.Program(bin, srcs)
+        env.Append(LIBS=libs)  # DANGER: will append the file over and over.
+    env.Program(bin, srcs)
 
 def run_tests(env, main_program_file, root, suffix):
+    root = os.path.normpath(root)
     for path in File.find_files_with_suffix(root, suffix):
         build_executable(env, path, main_program_file)
 
