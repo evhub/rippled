cd366e05e99d00214fe5395b24fff8f1ef3a053e
Tom Ritchford <tom@swirly.com>
2014-04-02 15:13:43 -0400

    Now we explicitly report missing Boost libraries.

--- src/beast/python/beast/util/Boost.py
+++ src/beast/python/beast/util/Boost.py
@@ -3,55 +3,59 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 import re
 
-BOOST_HOME = 'BOOST_HOME'
-MINIMUM_BOOST_VERSION = 1, 55, 0
-BOOST_VERSION_FILE = 'boost', 'version.hpp'
-BOOST_VERSION_MATCHER = re.compile(r'#define\s+BOOST_VERSION\s+(\d+)')
+HOME_ENV_VARIABLE = 'BOOST_HOME'
+MINIMUM_VERSION = 1, 55, 0
+VERSION_FILE = 'boost', 'version.hpp'
+LIBRARY_PATH_SEGMENT = 'stage', 'lib'
+VERSION_MATCHER = re.compile(r'#define\s+BOOST_VERSION\s+(\d+)')
 
 CANT_OPEN_VERSION_FILE_ERROR = """Unable to open boost version file %s.
 You have set the environment variable BOOST_HOME to be %s.
 Please check to make sure that this points to a valid installation of boost."""
 
 CANT_UNDERSTAND_VERSION_ERROR = (
-  "Didn't understand version string '%s' from file %s'")
+    "Didn't understand version string '%s' from file %s'")
 VERSION_TOO_OLD_ERROR = ('Your version of boost, %s, is older than the minimum '
                          'required, %s.')
 
 def _text(major, minor, release):
-  return '%d.%02d.%02d' % (major, minor, release)
+    return '%d.%02d.%02d' % (major, minor, release)
 
-def _boost_path():
+def _raw_boost_path():
     try:
-      path = os.environ[BOOST_HOME]
+      path = os.environ[HOME_ENV_VARIABLE]
       if path:
         return os.path.normpath(path)
     except KeyError:
-      pass
-    raise KeyError('%s environment variable is not set.' % BOOST_HOME)
+        pass
+    raise KeyError('%s environment variable is not set.' % HOME_ENV_VARIABLE)
 
 def _get_version_number(path):
-  version_file = os.path.join(path, *BOOST_VERSION_FILE)
-  try:
-    with open(version_file) as f:
-      for line in f:
-        match = BOOST_VERSION_MATCHER.match(line)
-        if match:
-          version = match.group(1)
-          try:
-            return int(version)
-          except ValueError:
-            raise Exception(CANT_UNDERSTAND_VERSION_ERROR %
-                            (version, version_file))
-  except IOError:
-    raise Exception(CANT_OPEN_VERSION_FILE_ERROR % (version_file, path))
+    version_file = os.path.join(path, *VERSION_FILE)
+    try:
+        with open(version_file) as f:
+            for line in f:
+                match = VERSION_MATCHER.match(line)
+                if match:
+                    version = match.group(1)
+                    try:
+                        return int(version)
+                    except ValueError:
+                        raise Exception(CANT_UNDERSTAND_VERSION_ERROR %
+                                        (version, version_file))
+    except IOError:
+        raise Exception(CANT_OPEN_VERSION_FILE_ERROR % (version_file, path))
 
 def _validate_version(v):
-  version = v // 100000, (v // 100) % 100, v % 100
-  if version < MINIMUM_BOOST_VERSION:
-    raise Exception(VERSION_TOO_OLD_ERROR % (
-      _text(*version), _text(*MINIMUM_BOOST_VERSION)))
+    version = v // 100000, (v // 100) % 100, v % 100
+    if version < MINIMUM_VERSION:
+        raise Exception(VERSION_TOO_OLD_ERROR % (
+    _text(*version), _text(*MINIMUM_VERSION)))
 
-def boost_path():
-  path = _boost_path()
-  _validate_version(_get_version_number(path))
-  return path
+def _boost_path():
+    path = _raw_boost_path()
+    _validate_version(_get_version_number(path))
+    return path
+
+PATH = _boost_path()
+LIBRARY_PATH = os.path.join(PATH, *LIBRARY_PATH_SEGMENT)
--- src/beast/python/beast/util/File.py
+++ src/beast/python/beast/util/File.py
@@ -2,13 +2,15 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os
 
+LIBRARY_PATTERN = 'lib%s.a'
+
 def first_line_starting_with(filename, prefix):
-  with open(filename, 'r') as contents:
-    for line in contents:
-      line = line.strip()
-      if line.startswith(prefix):
-        return line[len(prefix):].split()
-  return []
+    with open(filename, 'r') as contents:
+        for line in contents:
+            line = line.strip()
+            if line.startswith(prefix):
+                return line[len(prefix):].split()
+    return []
 
 def find_files_with_suffix(base, suffix):
     for parent, _, files in os.walk(base):
@@ -18,10 +20,20 @@ def find_files_with_suffix(base, suffix):
                 yield os.path.normpath(path)
 
 def child_files(parent, files):
-  return [os.path.normpath(os.path.join(parent, f)) for f in files]
+    return [os.path.normpath(os.path.join(parent, f)) for f in files]
 
 def sibling_files(path, files):
-  return child_files(os.path.dirname(path), files)
+    return child_files(os.path.dirname(path), files)
 
 def replace_extension(file, ext):
-  return os.path.splitext(file)[0] + ext
+    return os.path.splitext(file)[0] + ext
+
+def validate_libraries(path, libraries):
+    bad = []
+    for lib in libraries:
+        libfile = os.path.join(path, LIBRARY_PATTERN % lib)
+        if not os.path.isfile(libfile):
+            bad.append(libfile)
+    if bad:
+        libs = 'library' if len(bad) == 1 else 'libraries'
+        raise Exception('Missing %s: %s' % (libs, ', '.join(bad)))
