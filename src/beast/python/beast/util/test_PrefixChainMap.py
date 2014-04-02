a9904c546f5fed25550791808dcf15ff1fdd7a48
Tom Ritchford <tom@swirly.com>
2014-04-02 16:50:30 -0400

    Add a trivial unit test and a README saying how to run tests.

---
+++ src/beast/python/beast/util/test_PrefixChainMap.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from unittest import TestCase
+
+class test_PrefixChainMap(TestCase):
+  def testSimple(self):
+      pass
