cd9e217737c41e0bd70df6155a3c4627d0d89f68
evhub <evanjhub@gmail.com>
2014-07-09 14:38:44 -0700

    Adds xcode specific plist classes

--- src/beast/site_scons/site_tools/PList.py
+++ src/beast/site_scons/site_tools/PList.py
@@ -1,10 +1,21 @@
+# Info: ----------------------------------------------------------------------------
+
+
 """
 A tool for working with old-style NeXT plists.
 Necessary for working with .xcodeproj files.
 """
 
+
+# Imports: -------------------------------------------------------------------------
+
+
 from __future__ import absolute_import
 import hashlib
+
+
+# Data: ----------------------------------------------------------------------------
+
 
 def xsorted(*args, **kwargs):
     """Sorts Objects To Insure Deterministic Ordering."""
@@ -22,9 +33,13 @@ def xsorted(*args, **kwargs):
             elif isinstance(item, dictelem):
                 return ("d", item.atts["isa"], str(len(item.atts)))
             else:
-                return ("x", item)
+                return (repr(type(item)), item)
         kwargs["key"] = _key
     return sorted(*args, **kwargs)
+
+
+# Main: ----------------------------------------------------------------------------
+
 
 class converter(object):
     """Converts Objects Into NeXT plist Format."""
@@ -38,7 +53,7 @@ class converter(object):
     def convert(self, inputobject):
         """Converts A Python Object To NeXT plist Format."""
 
-        if isinstance(inputobject, rootobject):
+        if isinstance(inputobject, rootelem):
             return inputobject.header() +self.linesep+ self.convert(dict(inputobject))
 
         elif isinstance(inputobject, hexid):
@@ -48,13 +63,16 @@ class converter(object):
             return self.convert(dict(inputobject))
 
         elif isinstance(inputobject, (str, unicode)):
-            return self.addtabs(inputobject)
+            return inputobject
+
+        elif isinstance(inputobject, (int, long)):
+            return str(inputobject)
 
         elif isinstance(inputobject, list):
             out = [self.addtabs("(")]
             self.recursion += 1
             for item in xsorted(inputobject):
-                out.append(self.convert(item) + ",")
+                out.append(self.addtabs(self.convert(item)) + ",")
             self.recursion -= 1
             out.append(self.addtabs(")"))
             return self.linesep.join(out)
@@ -63,11 +81,7 @@ class converter(object):
             out = [self.addtabs("{")]
             self.recursion += 1
             for k,v in xsorted(inputobject.items()):
-                vs = self.convert(v).split(self.linesep, 1)
-                value = vs[0].rstrip()
-                if len(vs) > 1:
-                    value += self.linesep+vs[1]
-                out.append(self.convert(k) + " = " + value + ";" + self.linesep*(self.recursion <= 2))
+                out.append(self.addtabs(self.convert(k)) +" = "+ self.convert(v) +";"+ self.linesep*(self.recursion <= 2))
             self.recursion -= 1
             out.append(self.addtabs("}"))
             return self.linesep.join(out)
@@ -75,12 +89,15 @@ class converter(object):
         else:
             raise TypeError("Unable to process plist object "+repr(inputobject))
 
+
 class hexid(object):
     """A 96-bit Hex ID To Be Used In NeXT plists."""
-    def __init__(self, text, key):
+
+    def __init__(self, key, text="Generic hexid"):
         """Creates The Hex ID."""
-        self.hashout = hashlib.md5(text+key).hexdigest()[:24].upper()
+        self.hashout = hashlib.md5(text+" "+key).hexdigest()[:24].upper()
         self.comment = comment(text)
+
     def __str__(self):
         """Converts To A Python String."""
         out = self.hashout
@@ -88,47 +105,267 @@ class hexid(object):
             out += " "+str(self.comment)
         return out
 
+    def __eq__(self, other):
+        """Determines Whether Two Hex IDs Are The Same."""
+        return self.hashout == other
+
+
 class comment(object):
     """A Comment To Be Used In NeXT plists."""
+
     def __init__(self, text):
         """Creates The Comment."""
         self.text = text
+
     def __str__(self):
         """Converts To A Python String."""
-        return "/* " + self.text + " */"
+        return "/* " + self.text.replace("*/", "*_/")+ " */"
+
 
 class dictelem(object):
     """A Dictionary Element To Be Used In NeXT plists."""
+
     def __init__(self, isa):
         """Creates The Dictionary."""
-        self.atts = {}
-        self.atts["isa"] = str(isa)
+        self.atts = {
+            "isa": str(isa)
+            }
+
     def __getitem__(self, k):
         """Gets An Attribute."""
         return self.atts[k]
+
     def __setitem__(self, k, v):
         """Sets An Attribute."""
         self.atts[k] = v
+
+    def __delitem__(self, k):
+        """Deletes An Attribute."""
+        del self.atts[k]
+
     def __dict__(self):
         """Converts To A Python Dictionary."""
         return self.atts
 
-class rootobject(dictelem):
+
+class rootelem(dictelem):
     """The Root Element Of A NeXT plist."""
-    def __init__(self, key, version=46, encoding="UTF8"):
+
+    def __init__(self, rootObject, objectVersion=46, encoding="UTF8", archiveVersion=1, classes=None):
         self.encoding = str(encoding)
-        self.atts = {}
-        self.atts["archiveVersion"] = 1
-        self.atts["classes"] = {}
-        self.atts["objectVersion"] = str(version)
-        self.atts["objects"] = {}
-        self.atts["rootObject"] = hexid("Project object", key)
+        self.atts = {
+            "archiveVersion": archiveVersion,
+            "classes": classes or {},
+            "objectVersion": objectVersion,
+            "objects": {},
+            "rootObject": rootObject
+            }
+
     def __getitem__(self, k):
-        """Gets An Attribute."""
+        """Gets An Object."""
         return self.atts["objects"][k]
+
     def __setitem__(self, k, v):
-        """Sets An Attribute."""
+        """Sets An Object."""
         self.atts["objects"][k] = v
+
+    def __delitem__(self, k):
+        """Deletes An Object."""
+        del self.atts["objects"][k]
+
     def header(self):
         """Gets The Encoding Header."""
         return "// !$*"+self.encoding+"*$!"
+
+
+class PBXContainerItemProxy(dictelem):
+    """An XCode PBXContainerItemProxy Object."""
+
+    def __init__(self, containerPortal, remoteGlobalIDString, remoteInfo, proxyType=2):
+        """Creates The PBXContainerItemProxy."""
+        self.atts = {
+            "isa": "PBXContainerItemProxy",
+            "containerPortal": containerPortal,
+            "proxyType": proxyType,
+            "remoteGlobalIDString": remoteGlobalIDString,
+            "remoteInfo": remoteInfo
+            }
+
+
+class PBXFileReference(dictelem):
+    """An XCode PBXFileReference Object."""
+
+    def __init__(self, path, lastKnownFileType="text", sourceTree="<group>", fileEncoding=4):
+        """Creates The PBXFileReference."""
+        self.atts = {
+            "isa": "PBXFileReference",
+            "fileEncoding": fileEncoding,
+            "lastKnownFileType": lastKnownFileType,
+            "path": path,
+            "sourceTree": '"'+sourceTree+'"'
+            }
+
+
+class PBXGroup(dictelem):
+    """An XCode PBXGroup Object."""
+
+    def __init__(self, path=None, sourceTree="<group>"):
+        """Creates The PBXGroup."""
+        self.atts = {
+            "isa": "PBXGroup",
+            "children": [],
+            "sourceTree": '"'+sourceTree+'"'
+            }
+        if path is not None:
+            self.atts["path"] = path
+
+    def __getitem__(self, k):
+        """Gets A Child."""
+        return self.atts["children"][k]
+
+    def __setitem__(self, k, v):
+        """Sets A Child."""
+        self.atts["children"][k] = v
+
+    def append(self, v):
+        """Appends A Child."""
+        self.atts["children"].append(v)
+
+    def extend(self, v):
+        """Extends Children."""
+        self.atts["children"].extend(v)
+
+
+class PBXLegacyTarget(dictelem):
+    """An XCode PBXLegacyTarget Object."""
+
+    def __init__(self,
+                 name,
+                 buildConfigurationList,
+                 buildWorkingDirectory,
+                 buildToolPath,
+                 buildArgumentsString="",
+                 dependencies=None,
+                 buildPhases=None,
+                 productName=None,
+                 passBuildSettingsInEnvironment=1):
+        """Creates The PBXLegacyTarget."""
+
+        self.atts = {
+            "isa": "PBXLegacyTarget",
+            "buildArgumentsString": '"'+buildArgumentsString+'"',
+            "buildConfigurationList": buildConfigurationList,
+            "buildPhases": buildPhases or [],
+            "buildToolPath":buildToolPath,
+            "buildWorkingDirectory":buildWorkingDirectory,
+            "dependencies": dependencies or [],
+            "name": name,
+            "passBuildSettingsInEnvironment": passBuildSettingsInEnvironment,
+            "productName": productName or name
+            }
+
+
+class PBXNativeTarget(dictelem):
+    """An XCode PBXNativeTarget Object."""
+
+    def __init__(self):
+        """Creates The PBXNativeTarget."""
+        self.atts = {
+            "isa": "PBXNativeTarget"
+            }
+
+
+class PBXProject(dictelem):
+    """An XCode PBXProject Object."""
+
+    def __init__(self,
+                 targets,
+                 mainGroup,
+                 ProductGroup,
+                 ProjectRef,
+                 buildConfigurationList,
+                 ORGANIZATIONNAME,
+                 projectDirPath="",
+                 projectRoot="",
+                 hasScannedForEncodings=0,
+                 LastUpgradeCheck=0510,
+                 compatibilityVersion="XCode 3.2",
+                 developmentRegion="English",
+                 knownRegions=None):
+        """Creates The PBXProject."""
+
+        self.atts = {
+            "isa": "PBXProject",
+            "attributes": {
+                "LastUpgradeCheck": LastUpgradeCheck,
+                "ORGANIZATIONNAME": ORGANIZATIONNAME
+                },
+            "buildConfigurationList": buildConfigurationList,
+            "compatibilityVersion": '"'+compatibilityVersion+'"',
+            "developmentRegion": developmentRegion,
+            "hasScannedForEncodings": hasScannedForEncodings,
+            "knownRegions": knownRegions or ["en"],
+            "mainGroup": mainGroup,
+            "projectDirPath": '"'+projectDirPath+'"',
+            "projectReferences": [
+                {
+                    "ProductGroup": ProductGroup,
+                    "ProjectRef": ProjectRef
+                    }
+                ],
+            "projectRoot": '"'+projectRoot+'"',
+            "targets": targets
+            }
+
+
+class PBXReferenceProxy(dictelem):
+    """An XCode PBXReferenceProxy Object."""
+
+    def __init__(self, remoteRef, path, fileType, name=None, sourceTree="BUILT_PRODUCTS_DIR"):
+        """Creates The PBXReferenceProxy."""
+        self.atts = {
+            "isa": "PBXReferenceProxy",
+            "fileType": '"'+fileType+'"',
+            "path": path,
+            "remoteRef": remoteRef,
+            "sourceTree": sourceTree
+            }
+        if name is not None:
+            self.atts["name"] = name
+
+
+class XCBuildConfiguration(dictelem):
+    """An XCode XCBuildConfiguration Object."""
+
+    def __init__(self, name):
+        """Creates The XCBuildConfiguration."""
+        self.atts = {
+            "isa": "XCBuildConfiguration",
+            "buildSettings": {},
+            "name": name
+            }
+
+    def __getitem__(self, k):
+        """Gets A Setting."""
+        return self.atts["buildSettings"][k]
+
+    def __setitem__(self, k, v):
+        """Sets A Setting."""
+        self.atts["buildSettings"][k] = v
+
+    def __delitem__(self, k):
+        """Deletes A Setting."""
+        del self.atts["buildSettings"][k]
+
+
+class XCConfigurationList(dictelem):
+    """An XCode XCConfigurationList Object."""
+
+    def __init__(self, buildConfigurations, defaultConfigurationName, defaultConfigurationIsVisible=0):
+        """Creates The XCConfigurationList."""
+        self.atts = {
+            "isa": "XCConfigurationList",
+            "buildConfigurations": buildConfigurations,
+            "defaultConfigurationIsVisible": defaultConfigurationIsVisible,
+            "defaultConfigurationName": defaultConfigurationName
+            }
